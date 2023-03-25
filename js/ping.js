85cf4152d592cdac26e6c4ef0aed2b6e93a733ca
Tou <128892014+TheRealTou@users.noreply.github.com>
2023-03-25 17:44:41 +0700

    Update ping.js

--- js/ping.js
+++ js/ping.js
@@ -1,16 +1,24 @@
-const pingTimes = {
-  us: 100,
-  uk: 150,
-  fr: 200,
-  jp: 250
-};
-
-const countrySelect = document.getElementById('country');
-const checkPingButton = document.getElementById('check-ping');
-const pingResult = document.getElementById('ping-result');
-
-checkPingButton.addEventListener('click', function() {
-  const selectedCountry = countrySelect.value;
-  const pingTime = pingTimes[selectedCountry];
-  pingResult.innerText = `Your ping in ${countrySelect.options[countrySelect.selectedIndex].text} is ${pingTime}ms`;
-});
+function ping() {
+  var server = document.getElementById("server").value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://cors-anywhere.herokuapp.com/http://" + server, true);
+  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      document.getElementById("loading").style.display = "none";
+      if (xhr.status === 0) {
+        document.getElementById("result").innerHTML = "<p><strong>Error:</strong> Unable to connect to the server.</p>";
+      } else {
+        var time = new Date().getTime() - xhr.getResponseHeader("Date");
+        document.getElementById("result").innerHTML = "<p><strong>Success!</strong> Ping time to " + server + " is " + time + "ms.</p>";
+      }
+    }
+  };
+  xhr.onerror = function() {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("result").innerHTML = "<p><strong>Error:</strong> Unable to connect to the server.</p>";
+  };
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("loading").style.display = "block";
+  xhr.send();
+}
